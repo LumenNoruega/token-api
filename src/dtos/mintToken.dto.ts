2e75b78111738b790fcac3759ebd31d81607640d
Evelin <evelinrpaumgertner@hotmail.com>
2025-03-30 19:34:03 -0300

    Eliminando archivos duplicados

--- src/dtos/mintToken.dto.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { ApiProperty } from "@nestjs/swagger";
-
-export class MintTokenDto {
-  @ApiProperty({ type: String, required: true, default: "My Address" })
-  address: string;
-}
